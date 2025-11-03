d8f2a94832a91925546e8b7864e4bbbf11d8641e
Bruno Martins <88806986+BrunoMartinsGameDev@users.noreply.github.com>
2025-11-03 15:55:27 -0300

    Add new text element to App component

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Texto Alterado</Text>
+      <Text>Diego que alterou</Text>
       <StatusBar style="auto" />
     </View>
   );
